c9c69063471afb0c59f56744f381c91282d40a15
212115 <212115@1220-010-22>
2024-02-07 16:46:59 +0300

    hjhjhjh

--- Game/PagesWindows/HeroEditPage.xaml.cs
+++ Game/PagesWindows/HeroEditPage.xaml.cs
@@ -211,7 +211,7 @@ namespace Game.PagesWindows
                                 }
                                 return;
                             }
-                        }
+                        } 
                     }
                 }
             } 
